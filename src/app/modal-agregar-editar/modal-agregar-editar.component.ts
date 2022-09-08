a1870b64560562897c8ff8afacb9caaab36c5373
Walter Taño <tano.walter@hotmail.com>
2022-09-07 21:07:37 -0300

    incluida interfaz color y ciudad

--- src/app/modal-agregar-editar/modal-agregar-editar.component.ts
+++ src/app/modal-agregar-editar/modal-agregar-editar.component.ts
@@ -1,25 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ciudad } from '../Ciudad';
+import { Color } from '../Color';
+
 @Component({
   selector: 'app-modal-agregar-editar',
   templateUrl: './modal-agregar-editar.component.html',
   styleUrls: ['./modal-agregar-editar.component.sass']
 })
 export class ModalAgregarEditarComponent implements OnInit {
+  modo: 'Agregar' | 'Editar' = 'Agregar';
   titulo: string = 'Agregar Nota';
   textoBoton: string = 'Agregar';
-  modo: 'Agregar' | 'Editar' = 'Agregar';
-  ciudades: string[] = [
-    'Buenos Aires',
-    'Montevideo',
-    'Paris',
-    'Tokyo',
+
+  ciudades: Ciudad[] = [
+    { nombre: 'Buenos Aires', lat: '', long: '' },
+    { nombre: 'Montevideo', lat: '', long: '' },
+    { nombre: 'Paris', lat: '', long: '' },
+    { nombre: 'Tokyo', lat: '', long: '' }
   ];
-  colores: string[] = [
-    'Verde',
-    'Amarillo',
-    'Cielo'
-  ]
+
+  colores: Color[] = [
+    { nombre: 'Verde', selector: '' },
+    { nombre: 'Amarillo', selector: '' },
+    { nombre: 'Cielo', selector: '' }
+  ];
 
   constructor() { }
 
