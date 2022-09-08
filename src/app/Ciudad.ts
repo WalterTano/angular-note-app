e182fb0c5191a92d7bc21620e1ddd7229680a71a
Tomás Rama <tomas.rama@correo.ucu.edu.uy>
2022-09-07 21:05:22 -0300

    note, getclima, interface ciudad, terminados

---
+++ src/app/Ciudad.ts
@@ -0,0 +1,5 @@
+export interface Ciudad{
+    nombre: string;
+    lat: string;
+    long: string;
+}
--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Note } from '../Note';
 
 @Component({
   selector: 'app-note',
@@ -6,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./note.component.sass']
 })
 export class NoteComponent implements OnInit {
+  note: Note = {
+    id: "id",
+    clase: "clase",
+    titulo: "titulo",
+    ciudad: "ciudad",
+    cuerpo: "cuerpo",
+    temperatura: "temperatura",
+    fechaFormateada: "fechaform"
 
+  };
   constructor() { }
 
   ngOnInit(): void {
