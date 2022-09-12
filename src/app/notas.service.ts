407a86645effaf51d31da9e319fcf93c7082f0ba
Walter Taño <tano.walter@hotmail.com>
2022-09-12 18:59:08 -0300

    Añadí servicio de notas

---
+++ src/app/notas.service.ts
@@ -0,0 +1,38 @@
+import { Note } from './Note';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotasService {
+  notas?: Map<string, Note>;
+
+  constructor() {
+    this.notas = new Map<string, Note>();
+  }
+
+  obtenerNota(id: string): Note | undefined {
+    if (this.notas) {
+      return this.notas.get(id);
+    }
+    return undefined;
+  }
+
+  eliminarNota(id: string) {
+    if (this.notas) {
+      this.notas.delete(id);
+    }
+  }
+
+  crearNota(nota: Note) {
+    if (this.notas) {
+      this.notas.set(nota.id, nota);
+    }
+  }
+
+  editarNota(nota: Note) {
+    if (this.notas) {
+      this.notas.set(nota.id, nota);
+    }
+  }
+}
