627c37a17d443b11944fc6616a069e1bfb3cfaa5
Tomás Rama <tomas.rama@correo.ucu.edu.uy>
2022-09-12 19:22:59 -0300

    modificado temperatura

---
+++ src/app/mock-temperaturas.ts
@@ -0,0 +1,8 @@
+export const TEMPERATURAS: Map<string, string> = new Map(
+    [
+        ["Montevideo", "30"],
+        ["Paris", "10"],
+        ["Berlin", "20"],
+        ["Unión Soviética", "-30"]
+    ]
+);
