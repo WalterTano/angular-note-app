33b48a9e565b1c2f7ccdf8926b87cb69576b182a
Tomás Rama <tomas.rama@correo.ucu.edu.uy>
2022-09-12 20:17:11 -0300

    mock temperatura terminado

--- src/app/temperature.service.ts
+++ src/app/temperature.service.ts
@@ -24,19 +24,4 @@ export class TemperatureService {
     return of(data);
   }
 
-
-  /*
-  async getClima(date:Date, ciudad:Ciudad){
-    try {
-      let fecha = date.toISOString().split('T')[0];
-      let horaParseada = date.getHours();
-      let data = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${ciudad.lat}&longitude=${ciudad.long}&hourly=temperature_2m&start_date=${fecha}&end_date=${fecha}`);
-      let obj = await data.json();
-      return obj.hourly.temperature_2m[horaParseada];  
-    } catch (e) {
-      return undefined;
-    }
-    
-  }
-  */
 }
